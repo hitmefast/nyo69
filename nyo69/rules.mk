64fd199ef87ce764bf04576fd286d613b5e938db
hitmefast <97181204+hitmefast@users.noreply.github.com>
2024-09-27 00:11:10 +0200

    Add files via upload

---
+++ nyo69/rules.mk
@@ -0,0 +1,9 @@
+# This file intentionally left blank
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
+
+# Features turned off
+CONSOLE_ENABLE = no
+RGBLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = no
+AUDIO_ENABLE = no
