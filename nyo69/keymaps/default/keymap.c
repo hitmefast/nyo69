d82e92096d04540f2da42053a105688ddc0b709e
hitmefast <97181204+hitmefast@users.noreply.github.com>
2024-10-01 13:55:39 +0200

    Update 1

--- nyo69/keymaps/default/keymap.c
+++ nyo69/keymaps/default/keymap.c
@@ -2,6 +2,19 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "keymap_us_international.h"
+#include "keymap_spanish.h"
+#include "keymap_german.h"
+#include "keymap_portuguese.h"
+#include "keymap_jp.h"
+
+
+// 
+#define KC_MAC_UNDO LGUI(KC_Z)
+#define KC_MAC_CUT LGUI(KC_X)
+#define KC_MAC_COPY LGUI(KC_C)
+#define KC_MAC_PASTE LGUI(KC_V)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -17,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl │GUI│Alt │                        │Alt│ Fn│Ctl│ ← │ ↓ │ → │
      * └─────┴───┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_65_iso(
+    [0] = LAYOUT_69_iso(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_PGUP,
         KC_NUBS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGDN,
@@ -38,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │     │   │    │                        │   │   │   │BL1│BL-│BL0│
      * └─────┴───┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [1] = LAYOUT_65_iso(
+    [1] = LAYOUT_69_iso(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,
         _______, _______, KC_UP,   _______, _______, _______, KC_CALC, _______, _______, _______, _______, _______, _______,          _______,
         KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -59,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │     │   │    │                        │   │   │   │   │   │   │
      * └─────┴───┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [2] = LAYOUT_65_iso(
+    [2] = LAYOUT_69_iso(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
